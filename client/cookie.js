28439f29658be138195e264e864ac6af51ad8470
Naman <namansurana32@gmail.com>
2025-03-18 19:20:39 +0530

    Refactor cookie.js and index.html for improved formatting and readability

--- client/cookie.js
+++ client/cookie.js
@@ -5,7 +5,8 @@ function setCookie(name, value, days, secure = false) {
     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
     expires = "; expires=" + date.toUTCString();
   }
-  let cookie = name + "=" + (value || "") + expires + "; path=/; SameSite=Strict";
+  let cookie =
+    name + "=" + (value || "") + expires + "; path=/; SameSite=Strict";
   if (secure) {
     cookie += "; Secure";
   }
